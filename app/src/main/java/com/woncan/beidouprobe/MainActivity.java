b41df2cd73c9172a7d820ae408fe1884181a25e1
LiuZ <liuzhao@woncan.com>
2022-01-17 13:47:46 +0800

    适配906A

--- app/src/main/java/com/woncan/beidouprobe/MainActivity.java
+++ app/src/main/java/com/woncan/beidouprobe/MainActivity.java
@@ -1,36 +1,23 @@
 package com.woncan.beidouprobe;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ViewDataBinding;
-
 import android.content.Context;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.woncan.beidouprobe.databinding.ActivityMainBinding;
 import com.woncan.device.Device;
 import com.woncan.device.DeviceManager;
 import com.woncan.device.bean.DeviceInfo;
-import com.woncan.device.bean.DeviceMessage;
-import com.woncan.device.bean.Satellite;
 import com.woncan.device.bean.WLocation;
-import com.woncan.device.listener.DeviceInfoListener;
 import com.woncan.device.listener.LocationListener;
-import com.woncan.device.listener.MessageListener;
 import com.woncan.device.listener.NMEAListener;
-import com.woncan.device.listener.SatelliteListener;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
